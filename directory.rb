93a874190bfb571440ccfcdd9b81bc46387cb424
Emily Chai <echai93@gmail.com>
2017-02-28 18:53:21 +0000

    Added more information of the student: country of birth, height and hobbies

--- directory.rb
+++ directory.rb
@@ -1,17 +1,25 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an emtpy array
+  # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = $stdin.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
+    # get user input about more information?
+    puts "Now enter this student's country of birth: "
+    country = $stdin.gets.chomp
+    puts "Now enter this student's hobbies: "
+    hobbies = $stdin.gets.chomp
+    puts "Now enter this student's height: "
+    height = $stdin.gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, country: country, hobbies: hobbies, height: height, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    puts "Now enter another student's name (or press enter to finish)"
+    name = $stdin.gets.chomp
   end
   # return the array of students
   students
@@ -23,12 +31,10 @@ def print_header
 end
 
 def print(students)
-  i = 0
-  while i < students.count
-      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
-      i += 1
-    end
+  students.each_with_index do |student, i|
+      puts "#{i + 1} #{student[:name]} is from #{student[:country]} and is #{student[:height]} tall. Hobbies include #{student[:hobbies]} (#{student[:cohort]} cohort)"
   end
+end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
