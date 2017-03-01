1c9220c833f7022122d03ad4bdcc201f2db35ab2
Emily Chai <echai93@gmail.com>
2017-03-01 11:35:51 +0000

    Changed the input_students function so that the user is asked to input the cohort. Modified to supply a default value if the input is empty AND if the user makes a typo they are asked to try again until correct

--- directory.rb
+++ directory.rb
@@ -8,6 +8,21 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # get user input about more information?
+    puts "Now enter the cohort of this student: "
+    # convert input from string to a symbol
+    cohort = $stdin.gets.chomp.downcase.to_sym
+    # inputs to be expected
+    cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
+    # if input is empty give a default value
+    if cohort.empty?
+      cohort = :november
+    # if cohort does not include any of the values listed in cohorts, try again until it does 
+    elsif !cohorts.include? cohort
+        until cohorts.include? cohort
+          puts "Invalid cohort. Try again."
+          cohort = $stdin.gets.chomp.downcase.to_sym
+        end
+    end
     puts "Now enter this student's country of birth: "
     country = $stdin.gets.chomp
     puts "Now enter this student's hobbies: "
@@ -15,12 +30,12 @@ def input_students
     puts "Now enter this student's height: "
     height = $stdin.gets.chomp
     # add the student hash to the array
-    students << {name: name, country: country, hobbies: hobbies, height: height, cohort: :november}
+    students << {name: name, country: country, hobbies: hobbies, height: height, cohort: cohort}
     puts "Now we have #{students.count} students"
     # get another name from the user
     puts "Now enter another student's name (or press enter to finish)"
     name = $stdin.gets.chomp
-  end
+    end
   # return the array of students
   students
 end
@@ -32,7 +47,7 @@ end
 
 def print(students)
   students.each_with_index do |student, i|
-      puts "#{i + 1} #{student[:name]} is from #{student[:country]} and is #{student[:height]} tall. Hobbies include #{student[:hobbies]} (#{student[:cohort]} cohort)".center(75)
+      puts "#{i + 1}. #{student[:name]} is from #{student[:country]} and is #{student[:height]} tall. Hobbies include #{student[:hobbies]} (#{student[:cohort].capitalize} cohort)".center(75)
   end
 end
 
