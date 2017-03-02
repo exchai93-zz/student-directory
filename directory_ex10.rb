2461430e8d583a3cee23777f4c78f30a3e576794
Emily Chai <echai93@gmail.com>
2017-03-02 14:30:54 +0000

    Refactored code: made students an instance variable @ and extracted separate functions from input_students

---
+++ directory_ex10.rb
@@ -0,0 +1,71 @@
+# Refactoring
+
+# instance variable
+@students = [] # an empty array accessible to all methods
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+# extract the code that prints the menu into its own method
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+# extract the code that prints the students into a method
+def show_students
+  print_header
+  print_stuents_list 
+  print_footer
+end
+
+# extract code of the case statement that decides what to do based on what the user entered
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print
+  @students.each do |student|
+    puts "#{@student[:name]} (#{@student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{@students.count} great students."
+end
+
+interactive_menu
