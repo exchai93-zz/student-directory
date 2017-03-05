675d0928c9f1f1d35c8946875a7073be1e20e773
Emily Chai <echai93@gmail.com>
2017-03-05 23:25:30 +0000

    Writing a short program that reads its own source code: quine

---
+++ directory_ex14.8.rb
@@ -0,0 +1,5 @@
+# Write a short program that reads its own source code
+# (search StackOverflow to find out how to get the name of the currently executed file)
+# and prints it on the screen.
+
+puts File.read("directory_ex14.7.rb")
